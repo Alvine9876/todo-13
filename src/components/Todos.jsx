e736c625e6c1f50479b9a28b822b3d132cfcb3e9
Alvine Clive Munene <alvine.munene@student.moringaschool.com>
2025-04-15 14:14:34 +0300

    initial commit

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,36 +1,30 @@
 import React from "react";
 
-const Todos = ({todos, setTodos}) => {
+const Todos = ({ todos, setTodos }) => {
 
-    const handleClick = (e) => {
-        console.log("I am clicked");
-        console.log(e);
-        
-        
-    }
-
-    const handlDelete = (id) => {
+    const handleDelete = (id) => {
         let newArray = todos.filter((todo) => id !== todo.id);
         setTodos(newArray);
         console.log(newArray);
     }
 
-
-    if (todos.length == 0) {
-        return <p>No todos for today</p>
+    if (todos.length === 0) {
+        return <p>No todos for today</p>;
     }
-    
+
     return (
         <ul>
-            {todos.map((todo) => <li key={todo.id}>
-                <span>id: {todo.id}</span>
-                <span>body: {todo.body}</span>
-                <button onClick={() => handlDelete(todo.id)} >delete</button>
-            </li>
-            )}
+            {todos.map((todo) => (
+                <li key={todo.id}>
+                    <span>id: {todo.id}</span>
+                    <span> body: {todo.body}</span>
+                    <button onClick={() => handleDelete(todo.id)}>
+                        Delete
+                    </button>
+                </li>
+            ))}
         </ul>
-    )
+    );
 }
 
-
-export default Todos
+export default Todos;
